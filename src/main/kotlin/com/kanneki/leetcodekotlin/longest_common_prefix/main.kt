f3cb9f5af7937d1d12812083367b0d6167daca7f
Kanneki Chen <pearce712134@gmail.com>
2022-06-26 23:13:07 +0800

    add romanToInt , longestCommonPrefix

---
+++ src/main/kotlin/com/kanneki/leetcodekotlin/longest_common_prefix/main.kt
@@ -0,0 +1,125 @@
+package com.kanneki.leetcodekotlin.longest_common_prefix
+
+class main {
+
+    /**
+     * 參考別人提的方案,最佳解
+     */
+    fun longestCommonPrefix(strs: Array<String>): String {
+
+        if (strs.size <= 1) return strs.firstOrNull().orEmpty()
+        var index = 0
+        while (true) {
+            for (i in strs) {
+                if (index >= i.length || i[index] != strs[0][index]) {
+                    return strs[0].substring(0, index)
+                }
+            }
+            index++
+        }
+
+    }
+
+    /**
+     * 參考別人提的方案,一行解決
+     */
+    fun longestCommonPrefix2(strs: Array<String>): String {
+        return if(strs.size == 0){""}else{strs.reduce{a, b -> a.commonPrefixWith(b)}}
+    }
+
+    /**
+     * 笨方法
+     */
+    fun longestCommonPrefix3(strs: Array<String>): String {
+
+        var resultList = listOf<String>()
+
+        if (strs.size == 1) {
+            return strs.first()
+        }
+
+        for (i in strs.indices) {
+            when(i) {
+                0 -> {}
+                1 -> {
+                    val tmp = strs[0]
+                    for (j in 0..tmp.length) {
+                        if (strs[i].indexOf(tmp.substring(0, j)) == 0) {
+                            resultList += tmp.substring(0, j)
+                        }
+                    }
+                }
+                else -> {
+                    if (resultList.none()) {
+                        return ""
+                    }
+
+                    var tmp = listOf<String>()
+
+                    resultList.forEach { item ->
+                        if (item.isNotBlank() && strs[i].indexOf(item) == 0) {
+                            tmp += item
+                        }
+                    }
+
+                    resultList = tmp
+                }
+            } // end when
+        } // end for
+
+        if (resultList.none()) return ""
+        return resultList.sortedByDescending { it.length }.first()
+
+    }
+
+    // 寫到其他東西了
+    fun other(strs: Array<String>): String {
+        var tmpList = listOf<String>()
+        var sum = listOf<String>()
+
+        if (strs.size == 1) {
+            return strs.first()
+        }
+
+        for (i in strs.indices) {
+            when(i) {
+                0 -> {
+                    tmpList = strs[i].split("")
+                }
+                1 -> {
+                    var tmp = ""
+                    tmpList.forEach { item ->
+                        if (item.isNotBlank() && strs[i].indexOf(item) >= 0) {
+                            sum += item
+                            if (tmp.isNotBlank()) {
+                                tmp += item
+                            } else {
+                                tmp = item
+                            }
+
+                            if (strs[i].indexOf(tmp) >= 0) {
+                                sum += tmp
+                            }
+                        } else {
+                            tmp = ""
+                        }
+                    }
+                }
+                else -> {
+                    if (sum.none()) return ""
+                    var tmp = listOf<String>()
+                    sum.forEach { item ->
+                        if (strs[i].indexOf(item) >= 0) {
+                            tmp += item
+                        }
+                    }
+                    sum = tmp
+                }
+            } // end when
+        } // end for
+
+        if (sum.none()) return ""
+        return sum.sortedByDescending { it.length }.first()
+
+    }
+}
---
+++ src/main/kotlin/com/kanneki/leetcodekotlin/roman_to_int/main.kt
@@ -0,0 +1,84 @@
+package com.kanneki.leetcodekotlin.roman_to_int
+
+class main {
+
+    /**
+     * 目前個人解
+     */
+    fun romanToInt(s: String): Int {
+        var sum = 0
+        s.split("").map { stringChangerInt(it) }.run {
+            forEachIndexed { index, item ->
+                if (index < size -1 && item >= this[index + 1]){
+                    sum += item
+                } else {
+                    sum -= item
+                }
+            }
+        }
+
+        return sum
+    }
+
+    fun stringChangerInt(s: String): Int {
+        return when(s) {
+            "I" -> 1
+            "V" -> 5
+            "X" -> 10
+            "L" -> 50
+            "C" -> 100
+            "D" -> 500
+            "M" -> 1000
+            else -> 0
+        }
+    }
+
+    /**
+     * 他人不錯的解
+     */
+    fun romanToInt2(s: String): Int {
+        val hashMap:HashMap<String, Int> = hashMapOf(
+            "I" to 1,
+            "V" to 5,
+            "X" to 10,
+            "L" to 50,
+            "C" to 100,
+            "D" to 500,
+            "M" to 1000
+        )
+
+
+        var result = hashMap.get(s[s.length-1].toString())!!
+        for(i in s.length-2 downTo 0){
+            hashMap.get(s[i].toString())?.let{index1->
+                hashMap.get(s[i+1].toString())?.let{index2->
+                    if(index1<index2){
+                        result-= index1
+                    }else{
+                        result+= index1
+                    }
+                }
+            }
+        }
+        return result
+    }
+
+    /**
+     * 他人不錯的解
+     */
+    fun romanToInt3(s: String): Int {
+        val map = mutableMapOf(
+            'I' to 1, 'V' to 5, 'X' to 10, 'L' to 50, 'C' to 100, 'D' to 500, 'M' to 1000
+        )
+        var number = 0
+        var last = 1000
+        s.forEach {
+            val value = map[it] ?: 0
+            if (value > last) number -= last * 2
+            number += value
+            last = value
+        }
+
+        return number
+    }
+}
