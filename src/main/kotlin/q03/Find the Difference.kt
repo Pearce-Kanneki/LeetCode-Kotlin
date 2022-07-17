47229e06ee269e0dee11883df2f0b9502d760fc2
Kanneki Chen <pearce712134@gmail.com>
2022-07-17 19:38:05 +0800

    modify 389

--- src/main/kotlin/q03/Find the Difference.kt
+++ src/main/kotlin/q03/Find the Difference.kt
@@ -5,7 +5,27 @@ package q03
  */
 class `Find the Difference` {
 
+    /**
+     * 較快的解
+     */
     fun findTheDifference(s: String, t: String): Char {
+        val sum1 = s.sumOfChars()
+        val sum2 = t.sumOfChars()
+        return Math.abs(sum1 - sum2).toChar()
+    }
+
+    inline fun String.sumOfChars() : Int {
+        var sum = 0
+        for (c in this){
+            sum += c.toInt()
+        }
+        return sum
+    }
+
+    /**
+     *
+     */
+    fun findTheDifference1(s: String, t: String): Char {
 
         val check = HashMap<Char, Int>()
 
